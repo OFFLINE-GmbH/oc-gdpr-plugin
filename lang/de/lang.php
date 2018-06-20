4bbdc9dcab6946c29ae5668419984636f676eba9
Ayumi Hamasaki <asianspawebsite@gmail.com>
2018-06-20 14:11:18 +0100

    General Tidy up of Cookies Section
    General Tidy up of Cookies Section, need to update the German and French Lang Files

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,7 +1,7 @@
 <?php return [
     'plugin'          => [
-        'name'        => 'GDPR',
-        'description' => 'Mache deine Website GDPR konform',
+        'name'        => 'GDPR und ePrivacy',
+        'description' => 'Mache deine Website GDPR und ePrivacy konform',
     ],
     'common'          => [
         'enabled'       => 'Aktiviert',
@@ -60,8 +60,8 @@
     ],
     'settings'        => [
         'cookies'        => [
-            'label'       => 'Cookies',
-            'description' => 'Cookie-Verwaltung',
+            'label'       => 'Cookies and Modules',
+            'description' => 'Cookies and Modules Management',
         ],
         'cookie_consent' => [
             'label'                     => 'Cookie Consent',
@@ -163,8 +163,8 @@
             'name'                   => 'Gruppenname',
             'initial_status'         => 'Initialzustand',
             'initial_status_comment' => 'Diese Cookiegruppe ist standardmässig aktiv',
-            'required'               => 'Zustimmung erzwungen',
-            'required_comment'       => 'Die Cookies dieser Gruppe werden für die Funktionalität der Website benötigt und können nicht deaktiviert werden',
+            'required'               => 'Required for the Website to Function and Work',
+            'required_comment'       => 'The Cookies and/or Modules in this group are needed for the website to work properly and can therefore not be disabled by the user.',
             'slug'                   => 'Slug',
             'default_level'          => 'Dieses Level ist für den Besucher voreingestellt',
         ],
@@ -174,31 +174,41 @@
         ],
     ],
     'cookie'          => [
-        'fields' => [
-            'name_owner'                  => 'Name/Eigentümer',
+        'fields' => [       
+            'name_owner'                  => 'Owner',
+            'name_owner_comment'          => 'Owner Name of the Service e.g. Google Analytics',
             'code'                        => 'Code',
+            'code_comment'                => 'This is generated for the plugin to work (you dont need to touch this)',
             'intial_status'               => 'Dieses Cookie ist standardmässig aktiviert',
             'levels'                      => 'Cookie-Levels',
             'purpose'                     => 'Zweck',
-            'purpose_comment'             => 'Beschreibe die Verwendung, die Verarbeitung und allfällige Benachrichtigungen. Beispiel: «Bestimmt ob der Seitenaufruf des Besuchers zu Statistikzwecken aufgezeichnet wird»',
+            'purpose_comment'             => 'Under ePrivacy Law (Article 4, para 11 and article 13, para 1, C and para 2, F), you need to Describe the Purpose of the Use, Processing and any Notifications e.g. \'These cookies are used from AddThis social sharing widget in order to make sure you see the updated count when you share a page\'.',
             'contact_details_dpo'         => 'Kontaktdaten des «Data Controller»',
-            'contact_details_dpo_comment' => 'Gemäss ePrivacy Recht müssen Kontaktdaten des Data Controller für ein Cookie bekanntgegeben werden. Beispiel: Cloudflare, Inc. 101 Townsend St. San Francisco, CA 94107. Attention: Data Protection Officer, privacyquestions@cloudflare.com',
+            'contact_details_dpo_comment' => 'Under ePrivacy Law (Article 13, Para 1, A and Recital 42), you have to give the DPO Contact Address e.g. Cloudflare, Inc. 101 Townsend St. San Francisco, CA 94107. Attention: Data Protection Officer, privacyquestions@cloudflare.com (please note the email address is optional)',
             'contact_links_dpo'           => 'Links zum «Data Controller»',
-            'contact_links_dpo_comment'   => 'Gemäss ePrivacy Recht müssen Links zur Website des Data Controller für ein Cookie erstellt werden. Beispiel: Link zur Website, AGBs, Datenschutzerklärung, etc.',
+            'contact_links_dpo_comment'   => 'Under ePrivacy Law (Article 13, para 2, B, C and D), you need to give links for \'rights to complain to Supervisory authority and to access correct data policy\' this means linking to their Privacy Policy, Terms and Conditions, Cookie Privacy and Opt-Out Pages if they have them.',
+            'contact_links_dpo_prompt'    => 'Neuen Link hinzufügen',            
             'contact_links'               => [
                 'name' => 'Anzeigetext',
                 'href' => 'Linkziel (absolute URL)',
             ],
-            'cookie_levels'               => [
-                'name_comment'       => 'Anzeigename',
-                'identifier'         => 'Identifikation',
-                'identifier_comment' => 'Die ID des Cookies wie es im Browser gespeichert wird',
+            'cookie_levels'          => [
+                'purpose_prompt'     => 'Add Granular Purpose Levels',
+                'purpose_label'      => 'Granular Purpose Levels',
+                'section1_label'     => 'Helpful Tip for Granular Purpose Levels',
+                'section1_comment'   => 'Under ePrivacy Law (Recital 32 and Article 29 working party guidance nov 2017), Granular Opt-In for several Processes. This means having an off setting, the having a basic setting, then having a more advanced setting and so on up to a full setting. An example of this could be Google Analytics where we have an Off Setting, then a basic Tracking Setting, then a basic Tracking Setting plus Plugins, then finally a full setting with everything. The number of levels depends on what you are adding.',
+                'section2_label'     => 'Helpful Tip for Item Details',
+                'section2_comment'   => 'You can list as many item details as you want, if this is for a cookie make sure you list all the Cookie Identification Names, for example Google Analytics can have 8 or more different ones different ones. Also make sure you have equal number of Name (Cookie Identification), Duration/Expiry and Type fields. To find the list of Website Cookies in Google Chrome go to Dev Tools > Application > Cookies. You can also use the website: https://cookiepedia.co.uk/ to look-up Cookies.',
+                'name'               => 'Name (Cookie Identification)',
+                'name_comment'       => 'The Name of the of the Cookie Prefix code e.g. \'_ga\'',              
                 'provider'           => 'Anbieter',
-                'provider_comment'   => 'Gemäss ePrivacy Recht muss die Website oder Domain des Cookies angegebenw erden',
+                'provider_comment'   => 'Gemäss ePrivacy Recht muss die Website oder Domain des Cookies angegebenw erden. (please exclude the https://, www and / at the end)',
+                'purpose'            => 'Purpose',
+                'purpose_comment'    => 'Describe the Purpose of the Use at each granular level, Article 5 requires that consent is requested in a granular manner for \'specified, explicit\' purposes. (please write a SINGLE Paragraph)',                  
                 'expiry'             => 'Ablauf/Lebensdauer',
-                'expiry_comment'     => 'Gemäss ePrivacy Recht muss angegeben werden, wie lange das Cookie im Browser gespeichert bleibt',
+                'expiry_comment'     => 'Gemäss ePrivacy Recht muss angegeben werden, wie lange das Cookie im Browser gespeichert bleibt. e.g. SESSION, 3 monate, 2 jahre.',
                 'type'               => 'Typ',
-                'type_comment'       => 'Gemäss ePrivacy Recht muss der Typ dieses Cookies angegeben werden',
+                'type_comment'       => 'Under ePrivacy Law, you need to state what type of cookie or module is, to make things easier we have made a drop-down menu',
 
             ],
         ],
