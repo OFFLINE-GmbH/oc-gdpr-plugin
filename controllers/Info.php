5a12a1b389cc87b3f7c1905d9352713278db5f5a
Tobias Kündig <tobias@offline.swiss>
2018-06-26 08:37:10 +0200

    Refactored info section

--- controllers/Info.php
+++ controllers/Info.php
@@ -29,106 +29,42 @@ class Info extends Controller
 
     public function index()
     {
-        $config = $this->makeConfig('$/offline/gdpr/models/info/fields.yaml');
-		
-	 	$config->alias = 'info';
-
-        $config->model = new \OFFLINE\GDPR\Models\Info;
-
-        $info = $this->makeWidget('Backend\Widgets\Form', $config);
-
-        $this->vars['info'] = $info;		
+        $this->pageTitle = 'Info';
     }
 
     public function dataretention()
     {
-        $config = $this->makeConfig('$/offline/gdpr/models/dataretention/fields.yaml');
-		
-	 	$config->alias = 'dataretention';
-
-        $config->model = new \OFFLINE\GDPR\Models\DataRetention;
-
-        $dataretention = $this->makeWidget('Backend\Widgets\Form', $config);
-
-        $this->vars['dataretention'] = $dataretention;		
+        $this->pageTitle = 'Data retention';
     }
 
     public function bannerlaws()
     {
-        $config = $this->makeConfig('$/offline/gdpr/models/bannerlaws/fields.yaml');
-		
-	 	$config->alias = 'bannerlaws';
-
-        $config->model = new \OFFLINE\GDPR\Models\BannerLaws;
-
-        $bannerlaws = $this->makeWidget('Backend\Widgets\Form', $config);
-
-        $this->vars['bannerlaws'] = $bannerlaws;		
+        $this->pageTitle = 'Banner laws';
     }
 
     public function cookieconsent()
     {
-        $config = $this->makeConfig('$/offline/gdpr/models/cookieconsent/fields.yaml');
-		
-	 	$config->alias = 'cookieconsent';
-
-        $config->model = new \OFFLINE\GDPR\Models\CookieConsent;
-
-        $cookieconsent = $this->makeWidget('Backend\Widgets\Form', $config);
-
-        $this->vars['cookieconsent'] = $cookieconsent;		
+        $this->pageTitle = 'Cookie consent';
     }
 
     public function cookiesmodulesuserguide()
     {
-        $config = $this->makeConfig('$/offline/gdpr/models/cookiesmodulesuserguide/fields.yaml');
-		
-	 	$config->alias = 'cookiesmodulesuserguide';
-
-        $config->model = new \OFFLINE\GDPR\Models\CookiesModulesUserguide;
-
-        $cookiesmodulesuserguide = $this->makeWidget('Backend\Widgets\Form', $config);
-
-        $this->vars['cookiesmodulesuserguide'] = $cookiesmodulesuserguide;		
+        $this->pageTitle = 'Cookies and Modules User Guide';
     }
 	
     public function cookiesmodulessetup()
     {
-        $config = $this->makeConfig('$/offline/gdpr/models/cookiesmodulessetup/fields.yaml');
-		
-	 	$config->alias = 'cookiesmodulessetup';
-
-        $config->model = new \OFFLINE\GDPR\Models\CookiesModulesSetup;
-
-        $cookiesmodulessetup = $this->makeWidget('Backend\Widgets\Form', $config);
-
-        $this->vars['cookiesmodulessetup'] = $cookiesmodulessetup;		
+        $this->pageTitle = 'Cookie and Modules Setup';
     }	
 
     public function overallinfo()
     {
-        $config = $this->makeConfig('$/offline/gdpr/models/overallinfo/fields.yaml');
-		
-	 	$config->alias = 'overallinfo';
-
-        $config->model = new \OFFLINE\GDPR\Models\OverallInfo;
-
-        $overallinfo = $this->makeWidget('Backend\Widgets\Form', $config);
-
-        $this->vars['overallinfo'] = $overallinfo;		
+        $this->pageTitle = 'Information about GDPR and ePrivacy';
     }
 	
     public function dpolinks()
     {
-        $config = $this->makeConfig('$/offline/gdpr/models/dpolinks/fields.yaml');
-		
-	 	$config->alias = 'dpolinks';
-
-        $config->model = new \OFFLINE\GDPR\Models\DpoLinks;
-
-        $dpolinks = $this->makeWidget('Backend\Widgets\Form', $config);
-
-        $this->vars['dpolinks'] = $dpolinks;		
+        $this->pageTitle = 'Contact Links for DPO of Most Common Cookies';
     }	
 								
 }
