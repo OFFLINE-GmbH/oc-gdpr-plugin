50e707d3a9f7f3635feab6f02810696074d75a6b
Antonie Hogewoning <antonie.hogewoning@gmail.com>
2018-09-10 11:55:36 +0200

    split english language files to frontend and backend

---
+++ lang/en/frontend.php
@@ -0,0 +1,33 @@
+<?php return [
+    'common'          => [
+        'contact_details' => 'Contact details',
+        'links' => 'Links',
+        'save_settings'   => 'Save settings',
+    ],
+    'cookie_banner'   => [
+        'message'             => 'We use cookies to personalize content and to analyze our traffic. Please decide if you are willing to accept cookies from our website.',
+        'advanced_settings'   => 'Advanced Settings',
+        'decline'             => 'Decline',
+        'accept'              => 'Accept',
+    ],
+    'cookie_manager'  => [
+        'description' => 'Displays an overview and edit form for all the cookies you have defined in the backend settings',
+        'expiry'      => [
+            3  => 'Ask again in 3 months',
+            6  => 'Ask again in 6 months',
+            12 => 'Ask again in 1 year',
+            24 => 'Ask again in 2 years',
+            36 => 'Ask again in 3 years',
+        ],
+    ],
+    'cookie'=> [
+        'fields' => [
+            'cookie_levels'               => [
+                'name'                   => 'Name',
+                'provider'               => 'Provider',
+                'expiry'                 => 'Duration/Expiry',
+                'type'                   => 'Type',
+            ],
+        ]
+    ]
+];
