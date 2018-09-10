82acf6fc9b8566e92ccc72266a80b3aa2cff56d5
Antonie Hogewoning <antonie.hogewoning@gmail.com>
2018-09-10 12:41:39 +0200

    add dutch frontend translations

---
+++ lang/nl/frontend.php
@@ -0,0 +1,32 @@
+<?php return [
+    'common'          => [
+        'contact_details' => 'Contact informatie',
+        'links' => 'Links',
+        'save_settings'   => 'Instellingen opslaan',
+    ],
+    'cookie_banner'   => [
+        'message'             => 'We gebruiken cookies om bezoekersaantallen en conversie te analyseren. Kies hier of je onze cookies wilt accepteren.',
+        'advanced_settings'   => 'Instellingen',
+        'decline'             => 'Weigeren',
+        'accept'              => 'Accepteren',
+    ],
+    'cookie_manager'  => [
+        'expiry'      => [
+            3  => 'Opnieuw vragen over 3 maanden',
+            6  => 'Opnieuw vragen over 6 maanden',
+            12 => 'Opnieuw vragen over 1 jaar',
+            24 => 'Opnieuw vragen over 2 jaar',
+            36 => 'Opnieuw vragen over 3 jaar',
+        ],
+    ],
+    'cookie'=> [
+        'fields' => [
+            'cookie_levels'               => [
+                'name'                   => 'Naam',
+                'provider'               => 'Provider',
+                'expiry'                 => 'Geldigheid',
+                'type'                   => 'Type',
+            ],
+        ]
+    ]
+];
