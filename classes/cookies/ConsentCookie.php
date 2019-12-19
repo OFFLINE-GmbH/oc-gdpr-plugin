8478835808706174e3f8c89dfd2ac85a8822d548
Tobias Kündig <tobias@offline.swiss>
2019-12-19 07:23:52 +0100

    Added proper is_countable calls

--- classes/cookies/ConsentCookie.php
+++ classes/cookies/ConsentCookie.php
@@ -78,7 +78,7 @@ class ConsentCookie
     public function isAllowed($cookieCode, $level = 0)
     {
         $consent = $this->get();
-        if ($consent !== null && count($consent) < 1) {
+        if (is_countable($consent) && count($consent) < 1) {
             return false;
         }
 
@@ -88,7 +88,7 @@ class ConsentCookie
     public function allowedCookieLevel($cookieCode, $level = 0)
     {
         $consent = $this->get();
-        if (count($consent) < 1) {
+        if (is_countable($consent) && count($consent) < 1) {
             return -1;
         }
 
